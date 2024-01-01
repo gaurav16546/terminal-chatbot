8ad217309dad8f996da2231c2d241e9b6293b493
Gaurav Thapa <gaurav16547@gmail.com>
2024-01-01 21:15:21 +0545

    Added extra english parameter for training

--- bot.py
+++ bot.py
@@ -10,12 +10,23 @@ chatbot = ChatBot(
     "Chatpot",
     filters=[filters.get_recent_repeated_responses],
     logic_adapters=[
-        "chatterbot.logic.BestMatch",
-         'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.TimeLogicAdapter',
+        {
+            "import_path":"chatterbot.logic.BestMatch",
+            # 'default_response': 'I am sorry, but I do not understand.',
+            'maximum_similarity_threshold': 0.90
+            # "response_selection_method": chatterbot.response_selection.get_first_response
+        }
     ]
-    )
-trainer = ChatterBotCorpusTrainer("chatterbot.corpus.english")
+)
+corpus_trainer = ChatterBotCorpusTrainer(chatbot)
+corpus_trainer.train(
+    "chatterbot.corpus.english.conversations",
+    "chatterbot.corpus.english.greetings",
+    "chatterbot.corpus.english.science",
+    "chatterbot.corpus.english.computers",
+    "chatterbot.corpus.english.emotion",
+    "chatterbot.corpus.english.humor")
+
 # trainer = ListTrainer(chatbot)
 # cleaned_corpus = clean_corpus(CORPUS_FILE)
 # trainer.train(cleaned_corpus)
