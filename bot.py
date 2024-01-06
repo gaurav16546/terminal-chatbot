ed844e8e80d290cf35984b0158127459bc47161d
Gaurav Thapa <gaurav16547@gmail.com>
2024-01-06 20:39:41 +0545

    changed the loop where bot keeps asking quesion(now keyboar interrupt will stop the  program) and added unit conversion logic adapter:

--- bot.py
+++ bot.py
@@ -6,16 +6,16 @@ from cleaner import clean_corpus
 
 # CORPUS_FILE = "chat.txt"
 
-chatbot = ChatBot(
+chattbot = ChatBot(
     "Chatpot",
     filters=[filters.get_recent_repeated_responses],
     logic_adapters=[
         {
-            "import_path":"chatterbot.logic.BestMatch",
-            # 'default_response': 'I am sorry, but I do not understand.',
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand.',
             'maximum_similarity_threshold': 0.90
-            # "response_selection_method": chatterbot.response_selection.get_first_response
-        }
+        },
+                    "chatterbot.logic.UnitConversion",
     ]
 )
 corpus_trainer = ChatterBotCorpusTrainer(chatbot)
@@ -23,15 +23,20 @@ corpus_trainer.train(
     "chatterbot.corpus.english.conversations",
     "chatterbot.corpus.english.greetings",
     "chatterbot.corpus.english.science",
-    "chatterbot.corpus.english.computers",)
+    "chatterbot.corpus.english.computers",
+    "./chatterbot_corpus/data/computer_science.yml")
 
 # trainer = ListTrainer(chatbot)
 # cleaned_corpus = clean_corpus(CORPUS_FILE)
 # trainer.train(cleaned_corpus)
-exit_conditions = (":q","quit","exit")
 while True:
-    query = input("< ")
-    if query in exit_conditions:
+    try:
+        user_input = input()
+
+        bot_response = chattbot.get_response(user_input)
+
+        print(f"🤖 {bot_response}")
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
         break
-    else:
-        print(f"🪴 {chatbot.get_response(query)}")
