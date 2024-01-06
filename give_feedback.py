6418ab0d2000513ba2bdc991f63c94f2789e6ef9
Gaurav Thapa <gaurav16547@gmail.com>
2024-01-06 23:07:09 +0545

    fixed feedback loop now it will use lisTrainer to train bot using user input

--- give_feedback.py
+++ give_feedback.py
@@ -12,14 +12,13 @@ def get_feedback():
         print('Please type either "Yes" or "No"')
         return get_feedback()
 
-def feedback_loop(bot):
+def feedback_loop(bot,trainer):
     while True:
         try:
             print(">", end=" ")
-            input_statement = Statement(text=input())
-            response = bot.generate_response(
-                input_statement
-            )
+            user_input = input()
+            input_statement = Statement(text=user_input)
+            response = bot.get_response(input_statement)
 
             print('\n Is "{}" a coherent response to "{}"? \n'.format(
                 response.text,
@@ -27,11 +26,13 @@ def feedback_loop(bot):
             ))
             if get_feedback() is False:
                 print(f"🤖 'please input the correct one'\n")
-                print("> ",end=" ")
-                correct_response = Statement(text=input())
-                bot.learn_response(correct_response, input_statement)
+                correct_response = input()
+                correct_response_statement= Statement(text=correct_response)
+                # bot.learn_response(correct_response_statement, input_statement)
+                trainer.train([input_statement.text,correct_response_statement.text])
                 print(f"🤖 'Responses added to bot!'")
 
         # Press ctrl-c or ctrl-d on the keyboard to exit
         except (KeyboardInterrupt, EOFError, SystemExit):
+            print("\nExiting feedback loop...")
             break
