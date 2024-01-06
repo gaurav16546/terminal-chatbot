5060f5d1f82f0168a1388f1d7ad07518d7989b05
Gaurav Thapa <gaurav16547@gmail.com>
2024-01-06 21:04:44 +0545

    logging was implemented

--- bot.py
+++ bot.py
@@ -1,24 +1,34 @@
 # bot.py
 from chatterbot import ChatBot,filters
+import logging
+from chatterbot.trainers import UbuntuCorpusTrainer
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from cleaner import clean_corpus
 
 # CORPUS_FILE = "chat.txt"
-
+logging.basicConfig(level=logging.INFO)
 chattbot = ChatBot(
     "Chatpot",
     filters=[filters.get_recent_repeated_responses],
     logic_adapters=[
+         {
+            'import_path': 'chatterbot.logic.SpecificResponseAdapter',
+            'input_text': 'Bring me your father.',
+            'output_text': 'Ok, here is a link: https://chat.openai.com/'
+        },
         {
             'import_path': 'chatterbot.logic.BestMatch',
             'default_response': 'I am sorry, but I do not understand.',
             'maximum_similarity_threshold': 0.90
         },
                     "chatterbot.logic.UnitConversion",
+                    'chatterbot.logic.MathematicalEvaluation',
+                    'chatterbot.logic.TimeLogicAdapter',
     ]
 )
-corpus_trainer = ChatterBotCorpusTrainer(chatbot)
+trainer = UbuntuCorpusTrainer(chattbot)
+corpus_trainer = ChatterBotCorpusTrainer(chattbot)
 corpus_trainer.train(
     "chatterbot.corpus.english.conversations",
     "chatterbot.corpus.english.greetings",
@@ -31,7 +41,7 @@ corpus_trainer.train(
 # trainer.train(cleaned_corpus)
 while True:
     try:
-        user_input = input()
+        user_input = input(f"🤔")
 
         bot_response = chattbot.get_response(user_input)
 
